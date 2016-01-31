8a0f1520a49a117847a41b9def5406929b1f524c
Josh <pearlsteinj@me.com>
2016-01-31 15:27:48 -0500

    Fix

---
+++ opencv_qr/CMakeFiles/qr_video.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/Josh/Desktop/Senior Year 2nd Semester/CIS401/tempp/opencv_qr/video.cpp" "/Users/Josh/Desktop/Senior Year 2nd Semester/CIS401/tempp/opencv_qr/CMakeFiles/qr_video.dir/video.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/include/opencv"
+  "/usr/local/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
